c4b8ca871e66f460513d8a908cb79f464ca172ec
RachaelMuthoni <muthonirachael50@gmail.com>
2022-06-28 16:00:51 +0300

    My REST API commit

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const bodyParser = require('body-Parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', router)
-app.use('/users', router)
-app.use('/user/:email', router)
-app.use('/login', router)
+
 
 
 app.listen(PORT, () => {
